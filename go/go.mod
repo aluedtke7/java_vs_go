bdc8006d8e909ab9a229bfb529db3168f80f8eea
Andreas Lüdtke <andi.luedtke@gmx.de>
2024-03-24 10:58:53 +0100

    Dependencies updated

--- go/go.mod
+++ go/go.mod
@@ -1,9 +1,10 @@
 module tyt-rest
 
-go 1.17
+go 1.22
 
 require (
-	github.com/golang-jwt/jwt/v4 v4.4.0
-	github.com/gorilla/mux v1.8.0
-	github.com/urfave/negroni/v2 v2.0.2 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.0
+	github.com/gorilla/mux v1.8.1
 )
+
+require github.com/urfave/negroni/v2 v2.0.2 // indirect
--- kotlin-ktor/src/main/kotlin/de/test/Application.kt
+++ kotlin-ktor/src/main/kotlin/de/test/Application.kt
@@ -1,11 +1,11 @@
 package de.test
 
+import de.test.plugins.configureRouting
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import io.ktor.server.plugins.*
-import de.test.plugins.*
-import io.ktor.server.application.*
-import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.plugins.contentnegotiation.*
 import kotlinx.serialization.json.Json
 
 fun main() {
